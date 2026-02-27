ac1c887c78bd9e6d545cdaa255bf52e58d8387c6
Chiara Savoldi <58480311+ChiaraSav97@users.noreply.github.com>
2026-02-27 15:40:47 +0100

    Update errors.py

--- scripts/errors.py
+++ scripts/errors.py
@@ -14,17 +14,18 @@ ERRORS = {
     # --------------------------------
     "ERR_001": "parameters.ini not found or invalid",
     "ERR_002": "Missing required parameter",
-    "ERR_003": "outputPath invalid",
-    "ERR_004": "Invalid parameter value weight",
-    "ERR_005": "Invalid parameter value mode",
-    "ERR_006": "Invalid parameter value poi_category_osm",
-    "ERR_007": "Invalid parameter value poi_category_custom_name",
-    "ERR_008": "Invalid parameter value poi_category_custom",
-    "ERR_009": "Invalid parameter value park_gates_source",
-    "ERR_010": "park_gates_csv_path missing or invalid",
-    "ERR_011": "poi_category_custom_csv not found",
-    "ERR_012": "clip_layer_path not found",
-    "ERR_013": "grid_path not found"    
+    "ERR_003": "output_local_path invalid",
+    "ERR_004": "Missing required MinIO configuration: MINIO_ACCESS_KEY | MINIO_SECRET_KEY | MINIO_ENDPOINT_URL",
+    "ERR_005": "Invalid parameter value weight",
+    "ERR_006": "Invalid parameter value mode",
+    "ERR_007": "Invalid parameter value poi_category_osm",
+    "ERR_008": "Invalid parameter value poi_category_custom_name",
+    "ERR_009": "Invalid parameter value poi_category_custom",
+    "ERR_010": "Invalid parameter value park_gates_source",
+    "ERR_011": "park_gates_csv_path missing or invalid",
+    "ERR_012": "poi_category_custom_csv not found",
+    "ERR_013": "clip_layer_path not found",
+    "ERR_014": "grid_path not found"    
 }
 
 
